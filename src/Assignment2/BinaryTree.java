e90f984c01ce66c6b8e4bfc197d5e5e3d1c40903
Dan Laptop <danjeremynavarro@gmail.com>
2020-08-11 22:12:21 -0600

    added insert element method using recursion

--- src/Assignment2/BinaryTree.java
+++ src/Assignment2/BinaryTree.java
@@ -1,46 +1,47 @@
 package Assignment2;
 
-class BinaryTree<T>{
+class BinaryTree<T extends Comparable<T>>{
 
     Node<T> root = new Node<>();
 
-    public void addNode(){
-
+    public void add(T d){
+        if (root.data == null){
+            root.data = d;
+        } else {
+            insert(root, d);
+        }
     }
 
-    static class Node<T>{
-        private T data;
-        private Node<T> left;
-        private Node<T> right;
+    private Node<T> insert(Node<T> n, T d){
+        if (n == null) {
+            Node<T> newNode = new Node<>();
+            newNode.data = d;
+            return newNode;
+        } else if (n.data.compareTo(d) > 0) {
+            n.left = insert(n.left, d);
+        } else if (n.data.compareTo(d) < 0){
+            n.right = insert(n.right, d);
+        }
+        return n;
+    }
+
+    private Node<T> traverseLeft(Node<T> currentNode){
+        while(currentNode.left != null){
+            traverseLeft(currentNode.left);
+        }
+        return currentNode;
+    }
+
+
+    static class Node<T extends Comparable<T>>{
+        protected T data;
+        protected Node<T> left;
+        protected  Node<T> right;
 
         Node(){
             left = null;
             right = null;
             data = null;
-        }
-
-        public void setData(T d){
-            data = d;
-        }
-
-        public T getData(){
-            return data;
-        }
-
-        public void setLeft(Node<T> d){
-            left = d;
-        }
-
-        public void setRight(Node<T> d){
-            right = d;
-        }
-
-        public Node<T> getLeft(){
-            return left;
-        }
-
-        public Node<T> getRight(){
-            return right;
         }
     }
 }
--- src/Assignment2/testBinaryTree.java
+++ src/Assignment2/testBinaryTree.java
@@ -1,4 +1,19 @@
 package Assignment2;
 
 public class testBinaryTree {
+
+    public static void main (String[] args){
+        BinaryTree<Integer> test = new BinaryTree<>();
+        test.add(10);
+        test.add(2);
+        test.add(4);
+        test.add(14);
+        test.add(10);
+        test.add(15);
+        test.add(13);
+        test.add(1);
+        test.add(20);
+
+    }
+
 }
